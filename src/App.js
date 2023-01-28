08cccecbae90b59dd9d39b5311dd4b93c60d097b
Cypherfelix <felixmarvinonline@gmail.com>
2023-01-28 10:22:06 +0300

    create pages and link using reat-dom-router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Album from './pages/Album/Album';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="album">
+              <Route path=":albumId" element={<Album />} />
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
